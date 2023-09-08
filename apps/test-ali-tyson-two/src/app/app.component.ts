d766f72ecfce4a908463188e340f2a0274a4eb74
Charlie Greenman <charlie@razroo.com>
2023-09-08 16:55:10 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/test-ali-tyson-two/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-ali-tyson-two-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-ali-tyson-two';
+}
